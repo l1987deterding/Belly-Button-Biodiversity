1c041c2d123770e2182b843861adddb76aca1baf
Lydia Deterding <deterding.lydia@gmail.com>
2021-08-20 11:31:32 -0500

    Commit with comments

--- Code/static/js/app.js
+++ Code/static/js/app.js
@@ -1,8 +1,11 @@
+// create the function that will pull the data from samples through d3
 function show_charts(select_data){
     d3.json("samples.json").then ((data)=> {
+    // code to pull up data for each sample (which I called "each_choice")
     each_choice = data.samples.filter(sample => sample.id == select_data)
     each_choice = each_choice[0]
     console.log(each_choice)
+    // create bar chart
     var bar_data = [
         {
           x: each_choice.sample_values.slice(0,10).reverse(),
@@ -12,12 +15,14 @@ function show_charts(select_data){
           orientation: 'h'
         }
       ];
+      //give bar chart a title
       var barlayout={
           title: "Top Ten OTUs"
       }
+      //show bar chart on webpage
       Plotly.newPlot('bar', bar_data, barlayout)
 
-
+    // create bubble chart
     var bubble_data = [
         {
           x: each_choice.otu_ids,
@@ -31,34 +36,35 @@ function show_charts(select_data){
           }
         }
       ];
+      //give bubble chart a title
       var bubble_layout={
           title: "Top Ten OTUs"
       }
+      // show bubble chart on webpage
       Plotly.newPlot('bubble', bubble_data, bubble_layout)
 
-      //var demographics = {
-      //    data.metadata.filter(sample => sample.id == select_data)
-      //    demographics = demographics[0]
-      //    console.log(demographics) 
-      //}
-
+      //demographics = data.metadata.filter(sample => sample.id == select_data)
+      //demographics = demographics[0]
+      //console.log(demographics) 
+      
       //Plotly.newPlot('sample-metadata', demographics)
     })
-
-
-
 }
 
-// Fetch the JSON data and console log it
+
+// Fetch the JSON data and put it into the dropdown menu
 d3.json("samples.json").then ((data)=> {
 dropdown=d3.select("#selDataset")
     console.log(data);
+// change number in dropdown menu
 data.names.forEach(element => {
     dropdown.append("option").text(element).property("value",element)
 });
+// show first sample and its data when webpage loads
     show_charts(data.names[0])
 });
 
+// change charts for each selection
 function optionChanged(select_data){
     console.log(select_data)
     show_charts(select_data)
